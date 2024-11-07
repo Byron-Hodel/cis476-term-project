74b8dc98167e0f7eda20f96ce12af36e7e5db9d4
Untraditional <58677398+Untraditional@users.noreply.github.com>
2024-11-07 02:55:46 +0000

    Adding basic Sign-in and sign-up pages

---
+++ term-project-frontend/app/sign-in/page.tsx
@@ -0,0 +1,6 @@
+'use client';
+import SignIn from './SignIn'; // Adjust the path based on your project structure
+
+export default function Page() {
+  return <SignIn />;
+}
---
+++ term-project-frontend/app/sign-up/page.tsx
@@ -0,0 +1,6 @@
+'use client';
+import SignUp from './SignUp'; // Adjust the path based on your project structure
+
+export default function Page() {
+  return <SignUp />;
+}
